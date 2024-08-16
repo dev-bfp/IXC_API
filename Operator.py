6059f91b9ceb3b27cac6760432cf6949ace31300
Brian Freitas <brian_pereira98@hotmail.com>
2024-08-16 18:01:14 -0300

    update

--- Operator.py
+++ Operator.py
@@ -1,13 +1,13 @@
 from IXC_main import *
 
 
-filtro = (['status','=','A'],['id_produto_ativ','!=','95'])
+filtro = (['status','=','A'],['desconto_fidelidade','=',''])
 ret = ['id','razao','desconto_fidelidade','taxa_instalacao','id_produto_ativ']
 dados = IXC.get_info_IXC('cliente_contrato', return_list=ret, param=filtro)
 print(dados)
 
-# df = pd.DataFrame(dados['registros'],columns=ret)
-# df.to_excel('C:/Users/brian/OneDrive/dev-bfp/GitHub/IXC_API/generated files/contrato_fidelidade3.xlsx', index= False)
+df = pd.DataFrame(dados['registros'],columns=ret)
+df.to_excel('C:/Users/brian/OneDrive/dev-bfp/GitHub/IXC_API/generated files/contrato_taxafidelidade1.xlsx', index= False)
 
 
 # lista = []
